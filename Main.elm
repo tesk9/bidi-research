2592dc9d715cf3b498e54fce5a7807b51d1ff7cb
Tessa Kelly <tessakkelly@gmail.com>
2017-11-26 14:21:41 -0800

    Adds a border to each version of the header

--- Main.elm
+++ Main.elm
@@ -6,12 +6,13 @@ import Color
 import Css
 import Css.Colors
 import Element
-import Element.Attributes
 import Html.Attributes
 import Html.Styled
 import Html.Styled.Attributes
 import Style
+import Style.Border
 import Style.Color
+import Style.Font
 
 
 main : Program Never () msg
@@ -48,7 +49,12 @@ accessibleHtml : Html.Html msg
 accessibleHtml =
     Html.section []
         [ Html.h1
-            [ Html.Attributes.style [ ( "color", "blue" ) ] ]
+            [ Html.Attributes.style
+                [ ( "color", "blue" )
+                , ( "border", "1px solid gray" )
+                , ( "text-align", "center" )
+                ]
+            ]
             [ helloWorld ]
         ]
 
@@ -61,7 +67,7 @@ styleElements : Html.Html msg
 styleElements =
     Element.layout elementStyleSheet <|
         Element.h1 Header
-            [ Element.Attributes.alignLeft ]
+            []
             --NOTE: using `Element.html` is discouraged by the library author
             (Element.html helloWorld)
 
@@ -75,6 +81,10 @@ elementStyleSheet =
     Style.styleSheet
         [ Style.style Header
             [ Style.Color.text Color.blue
+            , Style.Color.border Color.gray
+            , Style.Border.solid
+            , Style.Border.all 1
+            , Style.Font.center
             ]
         ]
 
@@ -88,6 +98,11 @@ elmCss =
     Html.Styled.toUnstyled <|
         Html.Styled.section []
             [ Html.Styled.h1
-                [ Html.Styled.Attributes.css [ Css.color Css.Colors.blue ] ]
+                [ Html.Styled.Attributes.css
+                    [ Css.color Css.Colors.blue
+                    , Css.border3 (Css.px 1) Css.solid Css.Colors.gray
+                    , Css.textAlign Css.center
+                    ]
+                ]
                 [ Html.Styled.fromUnstyled helloWorld ]
             ]
