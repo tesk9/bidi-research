323d83718cee4aa30d49fd4e2694f7c9c1aa8d4e
Tessa Kelly <tessakkelly@gmail.com>
2017-11-26 12:42:00 -0800

    Adds elm css code

--- Main.elm
+++ Main.elm
@@ -2,12 +2,16 @@ module Main exposing (..)
 
 import Accessibility as Html
 import ArabicHtml exposing (..)
-import Color exposing (..)
-import Element exposing (..)
-import Element.Attributes exposing (..)
+import Color
+import Css
+import Css.Colors
+import Element
+import Element.Attributes
+import Html.Styled
+import Html.Styled.Attributes
 import Style
-import Style.Color as Color
-import Style.Font as Font
+import Style.Color
+import Style.Font
 
 
 main : Program Never () msg
@@ -26,8 +30,9 @@ view model =
             [ a_إنجليزي [ Html.text "اهلاً" ]
             , a_عربي [ Html.text ", world!" ]
             ]
-        , layout stylesheet <|
-            el Header [ alignLeft ] (text "Hello World!")
+        , Element.layout elementStyleSheet <|
+            Element.el Header [ Element.Attributes.alignLeft ] (Element.text "Hello World!")
+        , Html.Styled.toUnstyled logo
         ]
 
 
@@ -35,12 +40,32 @@ type Styles
     = Header
 
 
-stylesheet : Style.StyleSheet Styles variation
-stylesheet =
+elementStyleSheet : Style.StyleSheet Styles variation
+elementStyleSheet =
     Style.styleSheet
         [ Style.style Header
-            [ Color.text darkGrey
-            , Color.background white
-            , Font.size 5
+            [ Style.Color.text Color.darkGrey
+            , Style.Color.background Color.white
+            , Style.Font.size 5
             ]
         ]
+
+
+{-| Taken from the elm-css examples.
+-}
+logo : Html.Styled.Html msg
+logo =
+    Html.Styled.img
+        [ Html.Styled.Attributes.src "https://yt3.ggpht.com/-MTyGz2Ak6EE/AAAAAAAAAAI/AAAAAAAAAAA/4bxyjOzC690/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"
+        , Html.Styled.Attributes.css
+            [ Css.width (Css.px 200)
+            , Css.display Css.inlineBlock
+            , Css.padding (Css.px 20)
+            , Css.border3 (Css.px 5) Css.solid (Css.rgb 120 120 120)
+            , Css.hover
+                [ Css.borderColor Css.Colors.red
+                , Css.borderRadius (Css.px 10)
+                ]
+            ]
+        ]
+        []
